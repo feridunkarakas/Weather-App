02d205748309285a83fde0c84bd7b901a722cae9
Feridun Karakaş <feridunkarakas@gmail.com>
2026-04-29 17:09:49 +0300

    Proje oluşturuldu
    Proje oluşturuldu

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-sky-400">
+         <Weather />
+    </div>
+  );
+}
+
+export default App;
